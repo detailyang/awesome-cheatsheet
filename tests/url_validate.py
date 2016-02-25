3e8e20bc627a98243f2db007f9f11ac4f03e4a88
detailyang <detailyang@gmail.com>
2016-02-25 12:47:12 +0800

    add test for check url validation

--- tests/url_validate.py
+++ tests/url_validate.py
@@ -3,7 +3,7 @@
 # @Author: detailyang
 # @Date:   2016-02-25 11:23:59
 # @Last Modified by:   detailyang
-# @Last Modified time: 2016-02-25 12:35:21
+# @Last Modified time: 2016-02-25 12:47:01
 
 import re
 import sys
@@ -12,12 +12,14 @@ from requests import get
 url_re = re.compile('.*\((.*?)\)')
 
 for i in range(ord('a'), ord('z')+1):
-    with open('docs/{alphabet}.md'.format(alphabet=chr(i))) as f:
-        for line in f.readlines():
-            m = re.match(url_re, line)
+    file = 'docs/{alphabet}.md'.format(alphabet=chr(i))
+    with open(file) as f:
+        for line, content in enumerate(f.readlines()):
+            m = re.match(url_re, content)
             if m is None:
                 continue
             result = get(m.group(1))
             if result.status_code >= 400:
-                print('{url} return {code}'.format(url = m.group(1), code = result.status_code))
+                print('{file} line #{line} {url} return {code}'.format(file=file, line=line,
+                    url=m.group(1), code=result.status_code))
                 sys.exit(1)
